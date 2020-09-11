318ac6092ff53dba52cddff1d6bd1d0964a87a77
Vladimir <bibubl@yandex.com>
2020-09-11 18:25:59 +0300

    Реализовано движение, управление мышью, стрельба

---
+++ Assets/Scripts/Motion/FPSInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+
+public class FPSInput : MonoBehaviour
+{
+    public float Speed = 6.0f;
+    CharacterController charController;
+    public float Gravity = -9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Speed;
+        float deltaZ = Input.GetAxis("Vertical") * Speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, Speed) * Time.deltaTime;
+        movement.y = Gravity;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/Motion/MouseLook.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float YawSpeed = 3.0f;
+    public float PitchSpeed = 9.0f;
+    public float MinPitch = -45.0f;
+    public float MaxPitch = 45.0f;
+    public float rotationX = 0;
+    public enum RotationAxes
+    {
+        MouseXandY,
+        MouseX,
+        MouseY
+    }
+    public RotationAxes axes = RotationAxes.MouseXandY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if(rigidbody != null) rigidbody.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * YawSpeed, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * PitchSpeed;
+            rotationX = Mathf.Clamp(rotationX, MinPitch, MaxPitch);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+        else
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * PitchSpeed;
+            rotationX = Mathf.Clamp(rotationX, MinPitch, MaxPitch);
+
+            float delta = Input.GetAxis("Mouse X") * YawSpeed;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+    }
+}
